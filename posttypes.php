368d2e82238fe675ed3b02b6a8e6e84da790637f
Ivor Cheung <djssite1@hotmail.com>
2014-05-25 10:20:53 +0100

    Added array + args

--- posttypes.php
+++ posttypes.php
@@ -25,13 +25,39 @@
 
 function custom_project_posttypes(){
 
-  $args = array(
-    'public'             => true,
-    'label'              => 'Projects',
-    'menu_position'      => 5,
+  $labels = array(
+        'name'               => 'Projects',
+        'singular_name'      => 'Project',
+        'menu_name'          => 'Projects',
+        'name_admin_bar'     => 'Project',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Project',
+        'new_item'           => 'New Projects',
+        'edit_item'          => 'Edit Projects',
+        'view_item'          => 'View Projects',
+        'all_items'          => 'All Projects',
+        'search_items'       => 'Search Projects',
+        'parent_item_colon'  => 'Parent Projects:',
+        'not_found'          => 'No projects found.',
+        'not_found_in_trash' => 'No projects found in Trash.',
+    );
 
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'menu_icon'          => 'dashicons-archive',
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'projects' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => 5,
+        'supports'           => array( 'title', 'editor', 'excerpt', 'thumbnail' )
+    );
 
-  );
   register_post_type ('Projects', $args);
 
 }
